0010876935bff2e440f87595137f2e6697fa8ac5
Jhoebe <jhoebelim@gmail.com>
2026-05-31 16:16:33 +0800

    Add Supabase credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://vzylpnuvsuianmuwqsnr.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_SE1OPKy63BYHkZfQkbIb_A_nAgXKGTQ';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
