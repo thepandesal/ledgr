512bbc66c907b87e02682a319d854b2948683c32
Jhoebe <jhoebelim@gmail.com>
2026-07-05 15:57:43 +0800

    fix: refetch recordings immediately after add modal closes

--- app/(app)/space-detail.tsx
+++ app/(app)/space-detail.tsx
@@ -1006,7 +1006,7 @@ export default function SpaceDetailScreen() {
       </SafeAreaView>
 
       {showAddModal && (
-        <AddRecordingScreen inlineProps={{ spaceId: spaceId as string, spaceName: name as string, defaultDate: new Date().toISOString().split('T')[0], onClose: () => { setShowAddModal(false); queryClient.invalidateQueries({ queryKey: ['recordings', spaceId] }); } }} />
+        <AddRecordingScreen inlineProps={{ spaceId: spaceId as string, spaceName: name as string, defaultDate: new Date().toISOString().split('T')[0], onClose: () => { setShowAddModal(false); queryClient.refetchQueries({ queryKey: ['recordings', spaceId] }); } }} />
       )}
 
       {/* Date modal */}
